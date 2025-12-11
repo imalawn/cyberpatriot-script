0f8fc5f661ffd49fcb3b7f7b8e6e60a6f2ff3d1f
alawn <leckiqie@gmail.com>
2025-12-11 17:05:21 -0500

    finished for now

--- linux_script.sh
+++ linux_script.sh
@@ -5,11 +5,11 @@ if [ "$EUID" -ne 0 ]; then
   exit
 fi
 
-apt update && apt upgrade
+apt-get update && apt-get upgrade
 
 # SET UP FIREWALL
 read -p "Install and set up firewall? [y/n] " confirm; if [[ "$confirm" == [yY] ]]; then
-    apt install ufw
+    apt-get install ufw
     systemctl enable --now ufw
     ufw enable
 fi
@@ -20,7 +20,7 @@ echo "Remove bad packages"
 echo "-------------------"
 completed=0
 while [ "$completed" -eq 0 ]; do
-    bad_packages=(nmap zenmap apache2 nginx lighttpd wireshark tcpdump netcat-traditional nikto ophcrack)
+    bad_packages=(ssh vsftpd nmap zenmap apache2 nginx lighttpd wireshark tcpdump netcat-traditional nikto ophcrack zangband amule)
     declare -a to_delete
     declare -a skipped
 
@@ -45,8 +45,8 @@ while [ "$completed" -eq 0 ]; do
     read -p "Do you want to continue? [Y/n] " confirm
     if [[ "$confirm" == [yY] ]]; then
         unset skipped
-        apt purge -y "${to_delete[@]}"
-        apt autoremove
+        apt-get purge -y "${to_delete[@]}"
+        apt-get autoremove
         completed=1
     else
         echo "Try again:"
@@ -80,4 +80,41 @@ for name in $(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd); do
     fi
     echo ""
 done
-echo "Remember to add any users that were not included already."
+echo "Add any users that were not included already. Then, disable the guest account in settings."
+echo "To expire a user's password: sudo chage -d 0 username"
+read -p "Press ENTER to continue..."
+
+# MISC
+echo "-------------"
+echo "Miscellaneous"
+echo "-------------"
+echo "Scanning for prohibited files..."
+
+prohibited=$(find /home -type f \
+    \( -iname "*.mp3" -o -iname "*.mp4" -o -iname "*.avi" -o -iname "*.mkv" \
+       -o -iname "*.wav" -o -iname "*.flac" -o -iname "*.mov" \) )
+
+if [ -n "$prohibited" ]; then
+    echo "Prohibited files found:"
+    echo "$prohibited"
+    echo "Remove the files you deem to be unallowed."
+    read -p "Press ENTER to continue..."
+else
+    echo "No prohibited files detected."
+fi
+
+echo "A few editor windows will open. Add or modify the following values:"
+echo "/etc/login.defs: PASS_MAX_DAYS 90, PASS_MIN_DAYS 0, PASS_WARN_AGE 7"
+echo "/etc/pam.d/common-password, line containing pam_unix.so: remember=5 minlen=8"
+gedit /etc/login.defs
+gedit /etc/pam.d/common-password
+read -p "Press ENTER to continue...."
+echo ""
+echo "An editor window will open. Add or modify the following line:"
+echo "net.ipv4.tcp_syncookies = 1"
+gedit /etc/sysctl.conf
+read -p "Press ENTER to continue...."
+sudo sysctl -p
+
+echo "TO-DO:"
+echo "Complete tasks inside README, Secure SSH (/etc/ssh/sshd_config) and FTP (/etc/vsftpd/vsftpd.conf), enable automatic updates, enable security update list, secure web server/other services (if posible), find more prohibited apps"
